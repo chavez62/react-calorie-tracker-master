41662fc5507dbfea6a5186bcc33ea81a13c2339c
lchav62 <lchav62@wgu.edu>
2024-05-21 21:21:42 -0700

    Changed color scheme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,20 +8,20 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ resetDay, openModal }) => {
   return (
-    <div className="bg-pink-500 p-4 flex justify-between items-center text-white">
+    <div className="bg-blue-500 p-4 flex justify-between items-center text-white">
       <h1 className="text-2xl flex items-center">
         <FaUtensils className="mr-2" /> Calorie Tracker
       </h1>
       <div className="flex space-x-2">
         <button
           onClick={openModal}
-          className="bg-white text-pink-600 py-1 px-4 rounded flex items-center"
+          className="bg-white text-blue-500 py-1 px-4 rounded flex items-center"
         >
           Set Daily Limit
         </button>
         <button
           onClick={resetDay}
-          className="bg-white text-pink-600 py-1 px-4 rounded flex items-center"
+          className="bg-white text-blue-500 py-1 px-4 rounded flex items-center"
         >
           <FaRedo className="mr-2" /> Reset Day
         </button>
